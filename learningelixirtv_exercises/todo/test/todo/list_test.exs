07681392f739790284a1c713777cc5acf4938c9a
apedraza <augusto.pedraza08@gmail.com>
2017-06-22 07:20:32 -0300

    todo-exercise: Added basic Todo.List GenServer

---
+++ learningelixirtv_exercises/todo/test/todo/list_test.exs
@@ -0,0 +1,24 @@
+defmodule Todo.ListTest do
+  use ExUnit.Case
+
+  alias Todo.List
+  alias Todo.Item
+
+  setup do
+    {:ok, list} = List.start_link("Home")
+
+    {:ok, list: list}
+  end
+
+  test ".items returns the todos items on the list", %{list: list} do
+    assert List.items(list) == []
+  end
+
+  test ".add adds a item to the list", %{list: list} do
+    item = Item.new("Create a OTP app")
+
+    List.add(list, item)
+
+    assert List.items(list) == [item]
+  end
+end
