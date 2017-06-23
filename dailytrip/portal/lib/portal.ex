29feeb220fe7e39e615a40b07c1de2c6bc0145e4
apedraza <augusto.pedraza08@gmail.com>
2017-06-23 07:06:00 -0300

    Portal exercise: Added inspect Protocol implementation

--- dailytrip/portal/lib/portal.ex
+++ dailytrip/portal/lib/portal.ex
@@ -22,3 +22,28 @@ defmodule Portal do
     end
   end
 end
+
+defimpl Inspect, for: Portal do
+  def inspect(%Portal{left: left, right: right}, _) do
+    left_door = inspect(left)
+    right_door = inspect(right)
+
+    left_data = left
+                  |> Portal.Door.get
+                  |> Enum.reverse
+                  |> inspect
+
+    right_data = right
+                  |> Portal.Door.get
+                  |> inspect
+
+    max = max(String.length(left_door), String.length(left_data))
+
+    """
+    #Portal<
+      #{String.rjust(left_door, max)} <=> #{right_door}
+      #{String.rjust(left_data, max)} <=> #{right_data}
+    >
+    """
+  end
+end
