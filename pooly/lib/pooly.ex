db4ad9330bf2ed150d3a2950102bddaa47457910
apedraza <augusto.pedraza08@gmail.com>
2017-07-04 07:56:23 -0300

    Pooly: finalized first version

--- pooly/lib/pooly.ex
+++ pooly/lib/pooly.ex
@@ -1,18 +1,28 @@
 defmodule Pooly do
-  @moduledoc """
-  Documentation for Pooly.
-  """
+  use Application
 
-  @doc """
-  Hello world.
+  alias Pooly.Server
 
-  ## Examples
+  def start(_type, _args) do
+    pool_config = [mfa: {Pooly.SampleWorker, :start_link, []}, size: 5]
 
-      iex> Pooly.hello
-      :world
+    start_pool(pool_config)
+  end
 
-  """
-  def hello do
-    :world
+  def start_pool(pool_config) do
+    Pooly.Supervisor.start_link(pool_config)
+  end
+
+  def checkout do
+    Server.checkout
+  end
+
+  def checkin(worker_pid) do
+    Server.checkin(worker_pid)
+  end
+
+  def status do
+    Server.status
   end
 end
+
